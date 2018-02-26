9d2f7d7522f7fa1ff1073a77490011d7a02bb08c
FVW <brandnewdata@gmail.com>
2018-02-26 20:33:07 +0900

    SM_02_Install DirectX SDK & Install Steam

--- sdk/steamworksexample/htmlsurface.h
+++ sdk/steamworksexample/htmlsurface.h
@@ -1,4 +1,4 @@
-//========= Copyright © 1996-2008, Valve LLC, All rights reserved. ============
+//========= Copyright ?1996-2008, Valve LLC, All rights reserved. ============
 //
 // Purpose: Class for handling finding servers, getting their details, and displaying
 // them inside the game
